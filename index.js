cc716d6250470edf2d35b52738a7eafb6fa5f3f9
sanketmangle <72307769+sanketmangle@users.noreply.github.com>
2021-06-16 06:37:33 +0530

    new date & time
    source also added

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-console.log("XXX");
+
 //d81e79207fee41e9a862b96b92e82e21  https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=d81e79207fee41e9a862b96b92e82e21
 //https://gnews.io/api/v4/top-headlines?lang=mr&country=in&token=5b35849eaced6edb9927432501cf4499 ***marathi news api***
 let newsAccordion = document.getElementById('newsAccordion');
+const d = new Date().toLocaleString();
+document.getElementById("demo").innerHTML = d;
 const xhr = new XMLHttpRequest();
 xhr.open('GET', `https://gnews.io/api/v4/top-headlines?lang=mr&country=in&token=5b35849eaced6edb9927432501cf4499`, true);
-
-
 xhr.onload = function () {
     if (this.status === 200) {
         let json = JSON.parse(this.responseText);
@@ -19,13 +19,12 @@ xhr.onload = function () {
                                 <h2 class="mb-0">
                                 <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
                                     aria-expanded="false" aria-controls="collapse${index}">
-                                   <b>Breaking News ${index+1}:</b> ${element["title"]}
-                                </button>
+                                   <b>Breaking News ${index+1}:</b> ${element["title"]} - ${element.source.name}
                                 </h2>
                             </div>
 
                             <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsAccordion">
-                                <div class="card-body"> ${element["content"]} ${element["name"]}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
+                                <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
                             </div>
                         </div>`;
             newsHtml += news;
@@ -40,3 +39,4 @@ xhr.onload = function () {
 xhr.send()
 
 //https://gnews.io/api/v4/top-headlines?lang=mr&country=in&max=1&token=5b35849eaced6edb9927432501cf4499
+//https://gnews.io/api/v4/top-headlines?ltopic=health&lang=en&country=in&token=5b35849eaced6edb9927432501cf4499  **English News**
